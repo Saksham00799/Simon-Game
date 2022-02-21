128031e1a0960d326b21f4d631ffe78c387a1f96
saksham00799 <mj1.sri98@gmail.com>
2022-02-21 13:40:42 +0530

    Version 2

--- game.js
+++ game.js
@@ -2,14 +2,25 @@ var buttonColours=["red", "blue", "green", "yellow"];
 var gamePattern=[];
 var started=false;
 var level=0;
+var ruleOpen=false;
+$(".rules").hide();
 $(document).keypress(function(){
    if(!started){
+     if(ruleOpen===true){
+       $(".rules").hide();
+       $(".container").show();
+     }
       $("#level-title").text("Level "+level);
+      $(".btn-rules").hide();
       nextSequence();
       started=true;
    }
 });
-
+$("#btn-r").click(function(){
+  $(".rules").fadeToggle();
+  $(".container").fadeToggle();
+  ruleOpen=true;
+})
 function checkAnswer(currentLevel){
    if(gamePattern[currentLevel]===uesrClickedPattern[currentLevel]){
       console.log("success");
@@ -18,9 +29,14 @@ function checkAnswer(currentLevel){
       }
    }
    else{
+
       console.log("wrong");
       playSound("wrong");
+      ruleOpen=true;
       $("#level-title").text("Game Over, Press Any Key to Restart");
+      if(ruleOpen===true){
+        $(".btn-rules").show();
+      }
       $("body").addClass("game-over");
       setTimeout(function(){$("body").removeClass("game-over");},200);
       startover();
@@ -40,7 +56,7 @@ function nextSequence(){
    // uesrClickedPattern=[];
    level++;
    $("#level-title").text("Level "+level);
-   var randomNumber=Math.floor(Math.random()*4); 
+   var randomNumber=Math.floor(Math.random()*4);
    // console.log(randomNumber);
    var randomChosenColour=buttonColours[randomNumber];
    // console.log(randomChosenColour);
@@ -48,7 +64,7 @@ function nextSequence(){
    // console.log(gamePattern[0]);
    $("#" + randomChosenColour).addClass("next");
    setTimeout(() => {  $("#"+randomChosenColour).removeClass("next"); }, 100);
-   // $("#"+randomChosenColour).animateHighlight("#dd0000", 1000);  
+   // $("#"+randomChosenColour).animateHighlight("#dd0000", 1000);
    // var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
    console.log(randomChosenColour);
    playSound(randomChosenColour);
